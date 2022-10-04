f2b8f3c81a6e2a3062ccec3ed4d14d58f5949ba0
Anuk Hongwanna <pikaekung@gmail.com>
2022-10-04 12:08:53 +0700

    Add direction and event.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, fromEvent, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,39 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   @ViewChild('canvas') canvas!: ElementRef;
 
+  private gridSize = 30;
+  private fps = 10;
+  private direction = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+
   private ctx!: CanvasRenderingContext2D;
+  private snake = {
+    position: { x: 5, y: 5 },
+    action: [0, 0]
+  }
 
   ngAfterViewInit(): void {
     const canvas = this.canvas.nativeElement;
     this.ctx = canvas.getContext('2d');
 
-
+    this.initInputEvent();
     requestAnimationFrame(this.frame.bind(this));
+  }
+
+  private initInputEvent(): void {
+    fromEvent<KeyboardEvent>(document, 'keyup')
+      .pipe(
+        tap((e: KeyboardEvent) => {
+          if (e.key === 'ArrowUp') {
+            this.snake.action = this.direction[0];
+          } else if (e.key === 'ArrowRight') {
+            this.snake.action = this.direction[1];
+          } else if (e.key === 'ArrowDown') {
+            this.snake.action = this.direction[2];
+          } else if (e.key === 'ArrowLeft') {
+            this.snake.action = this.direction[3];
+          }
+        }),
+      ).subscribe();
   }
 
   private frame(): void {
@@ -24,27 +50,54 @@ export class AppComponent {
     this.render();
     const t1 = performance.now();
     this.drawLog(t1 - t0);
-    requestAnimationFrame(this.frame.bind(this))
+
+    setTimeout(() => {
+      requestAnimationFrame(this.frame.bind(this))
+    }, 1000 / this.fps);
   }
 
   private update(): void {
     // console.log(`Update: ${Date.now()}`)
+    this.snake.position.x += this.snake.action[0];
+    this.snake.position.y += this.snake.action[1];
+
+    // Check map
+    if (this.snake.position.x === 17) {
+      this.snake.position.x = 0;
+    }
+
+    if (this.snake.position.x === -1) {
+      this.snake.position.x = 17;
+    }
+
+    if (this.snake.position.y === 10) {
+      this.snake.position.y = 0;
+    }
+
+    if (this.snake.position.y === -1) {
+      this.snake.position.y = 10;
+    }
   }
 
   private render(): void {
-    // this.draw();
     // Clear rect
     this.ctx.clearRect(0, 0, 510, 300);
+
     this.drawGrid();
+
+    this.drawSnake();
+  }
+
+  private drawSnake(): void {
+    this.ctx.fillStyle = 'red';
+    this.ctx.fillRect(this.snake.position.x * this.gridSize, this.snake.position.y * this.gridSize, this.gridSize, this.gridSize);
   }
 
   private drawLog(timeMs: number): void {
     this.ctx.fillStyle = 'red';
     this.ctx.fillText(`Time: ${timeMs}`, 10, 15);
-    const t0 = performance.now();
-
+    this.ctx.fillText(`Position: (${this.snake.position.x}, ${this.snake.position.y})`, 10, 25);
   }
-
 
   private draw(): void {
     this.ctx.fillRect(25, 25, 100, 100);
@@ -64,7 +117,7 @@ export class AppComponent {
   }
 
   private drawGrid(): void {
-    const gridSize = 30;
+    const gridSize = this.gridSize;
     const gridHeight = 10;
     const gridWidth = 17;
     this.ctx.lineWidth = 0.1;
